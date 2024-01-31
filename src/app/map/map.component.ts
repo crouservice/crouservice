eb423d7305bac3444512fd75568b2056eefb4642
enimaloc <antoine@enimaloc.fr>
2024-01-31 15:59:25 +0100

    Correction de la map qui ne s'affiche pas

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -16,39 +16,21 @@ export class MapComponent implements OnInit {
     map.on('moveend', this.updateLocation);
     map.on('overlayadd', this.updateFilter);
     map.on('overlayremove', this.updateFilter);
+    map.locate();
 
-    leaf.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-      maxZoom: 19
+    leaf.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
     }).addTo(map);
-
-//    this.getLocation((position: GeolocationPosition) => {
-//      map.setView([position.coords.latitude, position.coords.longitude], 20);
-//    });
 
     let restaurant = leaf.layerGroup();
     leaf.marker([48.015357681471045, 0.16513664872329234]).addTo(restaurant).bindPopup('Vaurouzé');
     leaf.marker([48.017760514741795, 0.1539500722304242]).addTo(restaurant).bindPopup('Bartholdi');
     restaurant.addTo(map);
 
-    let hebergement = leaf.layerGroup()
+    let hebergement = leaf.layerGroup();
     leaf.marker([48.015991645087695, 0.16614303253937102]).addTo(hebergement).bindPopup('Vaurouzé');
     leaf.marker([48.01841058775789, 0.1537444306787454]).addTo(hebergement).bindPopup('Bartholdi');
-    hebergement.addTo(map);
-
-    leaf.control.layers({},
-      {
-        'Restaurants': restaurant,
-        'Hébergement': hebergement
-      }).addTo(map);
-  }
-
-  getLocation(success: PositionCallback, error?: PositionErrorCallback | null) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error);
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
+    leaf.control.layers({}, {'Restaurants': restaurant, 'Hébergement': hebergement}).addTo(map);
   }
 
   updateLocation() {
