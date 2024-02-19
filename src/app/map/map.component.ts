93f6b82739ad4ba2afe0a9d6321dbaec7a430bc5
enimaloc <antoine@enimaloc.fr>
2024-02-19 17:01:25 +0100

    Récupération des localisation via des méthodes

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -35,13 +35,15 @@ export class MapComponent implements OnInit {
     }).addTo(map);
 
     let restaurant = leaf.layerGroup();
-    leaf.marker([48.015357681471045, 0.16513664872329234], {icon: restaurantIcon}).addTo(restaurant).bindPopup('Vaurouzé');
-    leaf.marker([48.017760514741795, 0.1539500722304242], {icon: restaurantIcon}).addTo(restaurant).bindPopup('Bartholdi');
+    this.getRestaurantLocation().forEach(location => {
+      leaf.marker([location.lat, location.lng], {icon: restaurantIcon}).addTo(restaurant).bindPopup(location.label);
+    });
     restaurant.addTo(map);
 
     let hebergement = leaf.layerGroup();
-    leaf.marker([48.015991645087695, 0.16614303253937102], {icon: hebergementIcon}).addTo(hebergement).bindPopup('Vaurouzé');
-    leaf.marker([48.01841058775789, 0.1537444306787454], {icon: hebergementIcon}).addTo(hebergement).bindPopup('Bartholdi');
+    this.getHebergementLocation().forEach(location => {
+      leaf.marker([location.lat, location.lng], {icon: hebergementIcon}).addTo(hebergement).bindPopup(location.label);
+    });
     hebergement.addTo(map);
 
     leaf.control.layers({}, {'Restaurants': restaurant, 'Hébergement': hebergement}).addTo(map);
@@ -53,5 +55,21 @@ export class MapComponent implements OnInit {
 
   updateFilter() {
     console.log("#TODO update filter")
+  }
+
+  getHebergementLocation() {
+    console.log("#TODO get hebergement location")
+    return [
+      {label: "Vaurouzé", lat: 48.015991645087695, lng: 0.16614303253937102},
+      {label: "Bartholdi", lat: 48.01841058775789, lng: 0.1537444306787454}
+    ]
+  }
+
+  getRestaurantLocation() {
+    console.log("#TODO get restaurant location")
+    return [
+      {label: "Vaurouzé", lat: 48.015357681471045, lng: 0.16513664872329234},
+      {label: "Bartholdi", lat: 48.017760514741795, lng: 0.1539500722304242}
+    ]
   }
 }
