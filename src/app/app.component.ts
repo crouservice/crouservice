b84153b14f3ca9140574f79466bfa2d3c29d6bfb
LucasBarbet <123744989+LucasBarbet@users.noreply.github.com>
2024-03-12 09:16:19 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { BarreVertComponent } from './barre-vert/barre-vert.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    SearchComponent, 
+    BarreVertComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
