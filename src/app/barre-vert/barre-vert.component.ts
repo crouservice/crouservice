56f663dcb69b848341436a7d71d5a5e0364e552c
enimaloc <antoine@enimaloc.fr>
2024-03-13 14:00:01 +0100

    Fix, méthode manquante

--- src/app/barre-vert/barre-vert.component.ts
+++ src/app/barre-vert/barre-vert.component.ts
@@ -11,4 +11,16 @@ export class BarreVertComponent implements OnInit {
   ngOnInit(): void {
       console.log("Hello")
   }
+
+  acces_pmr(): void {}
+  parking(): void {}
+  laverie(): void {}
+  cuisine_collec(): void {}
+  douche_indiv(): void {}
+  kitchenette_indiv(): void {}
+  internet(): void {}
+  garage_velos(): void {}
+  securite(): void {}
+  restauration_sur_place(): void {}
+  salle_de_travail(): void {}
 }
