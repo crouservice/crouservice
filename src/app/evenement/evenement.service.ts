92f64a4aa3f4ae557759123ba596795fe29e2bc7
Antoine <antoine@enimaloc.fr>
2024-03-28 09:23:07 +0100

    Ajout des commentaires des evenements entre modules
    Co-authored-by: Braisetheghost <123772211+Braisetheghost@users.noreply.github.com>
    Co-authored-by: LucasBarbet <lucas.barbet72@gmail.com>
    Co-authored-by: Kylian Patault <123745029+KylianPatault@users.noreply.github.com>

--- src/app/evenement/evenement.service.ts
+++ src/app/evenement/evenement.service.ts
@@ -10,14 +10,25 @@ export class EvenementService {
   @Output() majLocationEvent: EventEmitter<LatLngBounds> = new EventEmitter<LatLngBounds>();
   @Output() gotoEvent: EventEmitter<LatLng> = new EventEmitter<LatLng>();
 
+  /**
+   * Mise à jour du filtre
+   */
   majFiltre() {
     this.majFiltreEvent.emit();
   }
 
+  /**
+   * Mise à jour de la localisation
+   * @param location Nouvelle localisation
+   */
   majLocation(location: LatLngBounds) {
     this.majLocationEvent.emit(location);
   }
 
+  /**
+   * Déplacement de la carte
+   * @param location Nouvelle position
+   */
   goto(location: LatLng) {
     this.gotoEvent.emit(location);
   }
